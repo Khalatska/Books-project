5ab1f1cf139a547f9ec0c801e393ffe93265aa7f
Khalatska <135690966+Khalatska@users.noreply.github.com>
2024-02-29 08:55:43 +0100

    fixed categories

--- src/js/categories.js
+++ src/js/categories.js
@@ -1,33 +1,55 @@
-// import {getCategoriesBooks} from './API';
-// const listCategories = document.querySelector('.list-categories');
+import {getCategoriesBooks} from './API';
+const listCategories = document.querySelector('.list-categories');
 
 
-// async function categoriesData (){
-//     try{
-//     const result = await getCategoriesBooks();
-//     const sorted = result.sort((a,b)=>a.list_name.localeCompare(b.list_name));
-//     const list = categoriesTemplate(sorted);
-//     listCategories.insertAdjacentHTML('beforeend', list);
-//     } catch(error){
+async function categoriesData (){
+    try{
+    const result = await getCategoriesBooks();
+    const sorted = result.sort((a,b)=>a.list_name.localeCompare(b.list_name));
+    const list = categoriesTemplate(sorted);
+    listCategories.insertAdjacentHTML('beforeend', list);
+    } catch(error){
+        console.error('Error fetching categories:', error);
+    }
+}
+ 
+
+function categoryTemplate ({list_name}){
+    const markup = `
+     <li class="item-categories">${list_name}</li>
+    `
+    return markup;
+}
+
+ 
+   function categoriesTemplate (res){
+   const markup =  res.map(categoryTemplate).join('');
+   return markup;
+}
+ categoriesData()
+
+
+// async function categoriesData() {
+//     try {
+//         const result = await getCategoriesBooks();
+//         const sorted = result.sort((a, b) => a.list_name.localeCompare(b.list_name));
+//         const list = await categoriesTemplate(sorted); // await the template function
+//         listCategories.insertAdjacentHTML('beforeend', list);
+//     } catch (error) {
 //         console.error('Error fetching categories:', error);
 //     }
 // }
 
-
-// function categoryTemplate ({list_name}){
-//     const markup = `
-//      <li class="item-categories">
-//      <button type="submit">${list_name}</button>
-//      </li>
-//     `
+// function categoryTemplate({ list_name }) {
+//     const markup = `<li class="item-categories">${list_name}</li>`;
 //     return markup;
 // }
 
 // async function categoriesTemplate (res){
-//    const markup =  res.map(categoryTemplate).join('');
-//    return markup;
-// }
-
-//  categoriesData()
+//     const markup =  res.map(categoryTemplate).join('');
+//     return markup;
+//  }
  
+// categoriesData();
+
  
