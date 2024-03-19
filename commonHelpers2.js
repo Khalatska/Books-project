21c69b4851cf3516525d104a0bbe97365f0f7a55
Khalatska <135690966+Khalatska@users.noreply.github.com>
2024-03-19 10:09:07 +0000

    Deploying to gh-pages from @ Khalatska/Books-project@d28d6cbe1b53f358b2f0552863066da82a2f9c53 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,26 +1,26 @@
-import{b as k,a as y}from"./assets/dark.-theme-f406ec29.js";const n=document.querySelector(".shopped-books"),t=JSON.parse(localStorage.getItem("shopping-list"))||[];n.insertAdjacentHTML("beforeend",p(t));function p(o){const e=window.innerWidth>768;return o.map(({book_image:s,title:i,list_name:h,description:a,author:g,buy_links:c,_id:b})=>{a||(a="Sorry, but this book does not have an accessible description. Try reading it on the website of one of the shops");const f=e?300:86,m=e?40:16;return`<li class="bought-book">
+import{b as y,a as v}from"./assets/dark.-theme-83c7fcc1.js";const n=document.querySelector(".shopped-books"),t=JSON.parse(localStorage.getItem("shopping-list"))||[];n.insertAdjacentHTML("beforeend",c(t));function c(o){const e=window.innerWidth>768;return o.map(({book_image:s,title:i,list_name:u,description:a,author:h,buy_links:r,_id:g,amazon_product_url:b})=>{a||(a="Sorry, but this book does not have an accessible description. Try reading it on the website of one of the shops");const f=r[1]?r[1].url:"",m=e?300:86,k=e?40:16;return`<li class="bought-book">
         <div class="img-container-shopping"> 
     <img src="${s}" alt="${i}" class="img-bought-book"/> </div>
     <div class="description-container">
     <h2 class="bought-book-title">${l(16,i)}</h2>
-    <p class="bought-book-name">${l(m,h)}</p>
-    <p class="bought-book-description">${l(f,a)}</p>
+    <p class="bought-book-name">${l(k,u)}</p>
+    <p class="bought-book-description">${l(m,a)}</p>
     <div class="author-shop-links">
-    <p class="bought-book-author">${l(15,g)}</p>
+    <p class="bought-book-author">${l(15,h)}</p>
     <ul class="shopping-shops">
       <li> 
-    <a href="${c.filter(({name:r})=>r==="Amazon").url}" target="_blanket" class="shopping-shops-link"><img src="${k}" alt="Amazon" class="img-shop-icon"/></a>
+    <a href="${b}" target="_blanket" class="shopping-shops-link"><img src="${y}" alt="Amazon" class="img-shop-icon"/></a>
     </li>
     <li> 
-    <a href="${c.filter(({name:r})=>r==="Apple").url}" target="_blank" class="shopping-shops-link"><img src="${y}" alt="Apple Books" class="img-shop-icon-apple"/></a>
+    <a href="${f}" target="_blank" class="shopping-shops-link"><img src="${v}" alt="Apple Books" class="img-shop-icon-apple"/></a>
     </li>
     </ul>
     </div>
     </div>
 
-    <button type="button" data-id=${b} class="btn-delete-book">
-    <svg class="trash-icon">
-    <use href="../img/sprite.svg#icon-trash"></use></svg></button>
+    <button type="button" data-id=${g} class="btn-delete-book">
+    <svg class="icon-burger">
+    <use href="../img/sprite.svg#icon-x-close"></use></svg></button>
  
-    </li>`}).join("")}d();function d(){document.querySelectorAll(".btn-delete-book").forEach(e=>{e.addEventListener("click",s=>{const i=s.currentTarget.dataset.id;v(i)})})}function v(o){const e=t.findIndex(s=>o===s._id);e!==-1&&(t.splice(e,1),localStorage.setItem("shopping-list",JSON.stringify(t)),u(),n.innerHTML="",n.insertAdjacentHTML("beforeend",p(t)),d())}u();function u(){const o=document.querySelector(".container-empty-list");t.length===0?(o.style.display="flex",n.style.display="none"):(o.style.display="none",n.style.display="flex")}function l(o,e){return e.length>o?e.slice(0,o)+"...":e}window.addEventListener("DOMContentLoaded",()=>{const o=document.querySelector(".support-container");function e(){window.innerWidth>1439?o.style.display="flex":o.style.display="none"}e(),window.addEventListener("resize",e)});
+    </li>`}).join("")}p();function p(){document.querySelectorAll(".btn-delete-book").forEach(e=>{e.addEventListener("click",s=>{const i=s.currentTarget.dataset.id;L(i)})})}function L(o){const e=t.findIndex(s=>o===s._id);e!==-1&&(t.splice(e,1),localStorage.setItem("shopping-list",JSON.stringify(t)),d(),n.innerHTML="",n.insertAdjacentHTML("beforeend",c(t)),p())}d();function d(){const o=document.querySelector(".container-empty-list");t.length===0?(o.style.display="flex",n.style.display="none"):(o.style.display="none",n.style.display="flex")}function l(o,e){return e.length>o?e.slice(0,o)+"...":e}window.addEventListener("DOMContentLoaded",()=>{const o=document.querySelector(".support-container");function e(){window.innerWidth>1439?o.style.display="flex":o.style.display="none"}e(),window.addEventListener("resize",e)});
 //# sourceMappingURL=commonHelpers2.js.map
