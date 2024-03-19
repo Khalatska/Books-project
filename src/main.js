f4748247c63e4e6ecb40e225085c7f6e015762de
Khalatska <135690966+Khalatska@users.noreply.github.com>
2024-03-19 14:25:53 +0100

    added scroll up btn

--- src/main.js
+++ src/main.js
@@ -5,3 +5,22 @@ import './js/best-sellers';
 import './js/modal-window';
 import './js/support-ukraine';
 import './js/dark.-theme';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const scrollUpButton = document.querySelector('.scroll-up');
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 600) {
+      scrollUpButton.classList.add('show');
+    } else {
+      scrollUpButton.classList.remove('show');
+    }
+  });
+
+  scrollUpButton.addEventListener('click', () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  });
+});
