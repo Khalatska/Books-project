4018a0e8c0b32c3d160a394e81358ad3a7264f0d
Khalatska <135690966+Khalatska@users.noreply.github.com>
2024-03-19 15:04:48 +0100

    fixed scroll

--- src/js/header.js
+++ src/js/header.js
@@ -1,14 +1,15 @@
-const refs = { 
-closeMenuElem : document.querySelector('.btn-close-menu'),
-openMenuElem : document.querySelector('.btn-open-menu'),
-modalMenuElem : document.querySelector('.mob-menu'),
+const refs = {
+  closeMenuElem: document.querySelector('.btn-close-menu'),
+  openMenuElem: document.querySelector('.btn-open-menu'),
+  modalMenuElem: document.querySelector('.mob-menu'),
 };
 
-refs.closeMenuElem.addEventListener('click', ()=>{
+refs.closeMenuElem.addEventListener('click', () => {
   refs.modalMenuElem.classList.remove('is-visible');
-})
+  document.body.style.overflow = 'hidden';
+});
 
-refs.openMenuElem.addEventListener( 'click', ()=>{
-    refs.modalMenuElem.classList.add('is-visible');
-})
-
+refs.openMenuElem.addEventListener('click', () => {
+  refs.modalMenuElem.classList.add('is-visible');
+  document.body.style.overflow = 'visible';
+});
